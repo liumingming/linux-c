ec6ccd2362bc6b2ed8c8e1aea99952ba04a54179
liumingming <liuming1987@gmail.com>
2020-05-05 10:11:54 +0800

    fix

---
+++ fileio.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+char buf[] = "abcdefg";
+char buf2[8];
+
+int main(void)
+{
+    int fd;
+    fd = open("file.txt", O_RDWR | O_APPEND);
+    if (fd < 0) {
+        printf("open filed\n");
+        exit(-1);
+    }
+
+    if (write(fd, buf, sizeof(buf)) != sizeof(buf)) {
+        printf("write failed\n");
+        exit(-1);
+    }
+    printf("write successful\n");
+    lseek(fd, 0, SEEK_SET);
+    if (read(fd, buf2, 8) == -1) {
+        printf("read failed\n");
+    }
+
+    printf("read successful, %s\n", buf2);
+
+    return 0;
+}
