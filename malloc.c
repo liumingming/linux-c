cb4694ccad4e53b4e82a2ff978d190965ac93691
liumingming3 <liuming1987@gmail.com>
2020-11-29 09:12:57 +0800

    add malloc

---
+++ malloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_SIZE 10
+
+int main(void) {
+    int *arr = malloc(sizeof(int)  * MAX_SIZE);
+
+    if (arr == NULL) {
+        printf("malloc memory filead");
+    }
+
+    for (int i = 0; i <  MAX_SIZE;  i++)  {
+        arr[i] = i;
+    }
+
+    for (size_t i = 0; i < MAX_SIZE; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    free(arr);
+}
